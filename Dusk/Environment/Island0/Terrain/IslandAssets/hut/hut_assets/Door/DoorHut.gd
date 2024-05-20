57e7a69d5306e46cceab092078893b49050fce52
Zoe <cmdrprotogen27@gmail.com>
2024-05-20 12:32:06 +0200

    fixing stuff

--- Dusk/Environment/Island0/Terrain/IslandAssets/hut/hut_assets/Door/DoorHut.gd
+++ Dusk/Environment/Island0/Terrain/IslandAssets/hut/hut_assets/Door/DoorHut.gd
@@ -11,19 +11,12 @@ func use_door():
 	
 	if (door_open):
 		animation_player.play("CloseDoor")
+		get_parent_node_3d().get_node("DoorOpen").play()
 		door_open = false
 	else:
 		animation_player.play("OpenDoor")
+		get_parent_node_3d().get_node("DoorClose").play()
 		door_open = true
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_interactable_interact():
