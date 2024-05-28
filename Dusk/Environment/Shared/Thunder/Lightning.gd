cc2f87f8163f8361db133469914294b4e5eed8dd
Zoe <cmdrprotogen27@gmail.com>
2024-05-28 14:47:19 +0200

    lightning

--- Dusk/Environment/Shared/Thunder/Lightning.gd
+++ Dusk/Environment/Shared/Thunder/Lightning.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+class_name Lightning
+
+signal lightning_finished(instance : Lightning)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,5 +16,11 @@ func lightning():
 	await get_tree().create_timer(0.09).timeout
 	$Sprite3D.hide()
 	$OmniLight3D.hide()
+	$Timer.start()
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+
+func _on_timer_timeout():
+	lightning_finished.emit(self)
--- Dusk/Environment/Shared/Thunder/LightningSpawner.gd
+++ Dusk/Environment/Shared/Thunder/LightningSpawner.gd
@@ -6,40 +6,64 @@ var player : CharacterBody3D
 const lightning_resource : String = "res://Environment/Shared/Thunder/LightningAndThunder.tscn";
 
 @export
-var spawn_radius : float = 100
+var spawn_radius : float = 200
+
+@export
+var min_spawn_time : float = 2
+
+@export
+var max_spawn_time : float = 40
+
+@onready
+var timer : Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent_node_3d().get_parent_node_3d().get_node("Player")
+	timer.start(get_random_time())
 
+func get_random_time() -> float:
+	return randf_range(min_spawn_time, max_spawn_time)
 
 func spawn_lightning():
-	var player_position
+	var player_position : Vector3
+	
 	if (player == null):
 		player_position = global_position
 	else:
 		player_position = player.global_transform.origin
 	
-	var random_point = Vector3()
-	
-	# Generate a random point within the spawn_radius
-	var angle = randf() * 2.0 * PI
-	var distance = randf() * spawn_radius
-	random_point.x = player_position.x + distance * cos(angle)
-	random_point.z = player_position.z + distance * sin(angle)
-	
-	# Set the Y coordinate to be above the player
-	random_point.y = player_position.y + 50.0 # Adjust height as needed
+	var random_position : Vector3 = get_random_position(player_position)
 	
 	# Instance and position the lightning
-	var lightning_instance = preload(lightning_resource)
-	lightning_instance.instantiate()
-	lightning_instance.global_transform.origin = random_point
+	var lightning_scene := preload(lightning_resource)
+	
+	var lightning_instance : Lightning = lightning_scene.instantiate()
+	
 	get_tree().current_scene.add_child(lightning_instance)
+	
+	lightning_instance.global_position = random_position
 	
 	lightning_instance.lightning()
 	
-	print("Lightning spawned at: ", random_point)
+	lightning_instance.lightning_finished.connect(remove_lightning_instance)
+
+func remove_lightning_instance(instance : Lightning) -> void:
+	get_tree().current_scene.remove_child(instance)
+
+func get_random_position(player_position : Vector3) -> Vector3:
+	var random_direction_normalized : Vector2 = Vector2(randf_range(-1, 1), randf_range(-1, 1))
 	
+	var player_position_2d : Vector2 = Vector2(player_position.x, player_position.z)
+	
+	var random_direction : Vector2 = random_direction_normalized * spawn_radius
+	
+	var random_location = player_position_2d + random_direction
+	
+	return Vector3(random_location.x, 30, random_location.y)
 
 
+
+func _on_timer_timeout():
+	spawn_lightning()
+	timer.start(get_random_time())
