a98a42fa486189e5904d63cd38b344f4bdf71e08
Zoe <cmdrprotogen27@gmail.com>
2024-06-04 08:55:44 +0200

    started work on fixing the island sound controller to actually work

--- Dusk/Environment/Shared/IslandSoundController.gd
+++ Dusk/Environment/Shared/IslandSoundController.gd
@@ -6,11 +6,25 @@ var player : Node3D
 @onready
 var ambiant_sound_player : AmbientSoundPlayer
 
+@onready
+var island0 : Node3D
+
+@onready
+var island1 : Node3D
+
+@onready
+var island2 : Node3D
+
+@onready
+var island3 : Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent_node_3d().get_parent_node_3d().get_node("Player")
 	ambiant_sound_player = get_parent_node_3d().get_parent_node_3d().get_node("AmbientSound")
+	
+	island0 = get_parent_node_3d().get_node("Island0Terrain")
+	island1 = get_parent_node_3d().get_node("Island1Terrain")
 
 func distance_to_player() -> float:
 	return (global_position - player.global_position).length()
